6d9f82f88f476bcdafd7dc832588741c20cbf8d0
KhemphumKhunleang <114760880+KhemphumKhunleang@users.noreply.github.com>
2025-05-31 21:50:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function saySorry() {
+  window.location.href = "thankyou.html";
+}
+function saynotSorry() {
+  window.location.href = "notthankyou.html";
+}
+function saynotSorry2() {
+  window.location.href = "notthankyou2.html";
+}
+function saynotSorry3() {
+  window.location.href = "notthankyou3.html";
+}
